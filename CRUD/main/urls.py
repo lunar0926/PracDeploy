7c6f3fbaf8576022678a4161898876b3bff0bc45
lunar0926 <lunar0926@gmail.com>
2021-05-27 19:01:04 +0900

    배포하자

--- CRUD/main/urls.py
+++ CRUD/main/urls.py
@@ -7,7 +7,7 @@ app_name = "main"
 urlpatterns=[
 
     
-    path('/create',views.create,name="create"),
+    path('create',views.create,name="create"),
     path('detail/<int:write_id>',views.detail,name="detail"),
     path('update/<int:write_id>',views.update,name="update"),
     path('delete/<int:write_id>',views.delete, name="delete"),
